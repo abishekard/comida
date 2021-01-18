ca4269f6d89b8382c3a9085c822894f5b9818d03
abishek kumar <abishek.ard@gmail.com>
2021-01-19 00:45:44 +0530

    homePage push

--- app/Http/Controllers/Customer/HomePageController.php
+++ app/Http/Controllers/Customer/HomePageController.php
@@ -10,7 +10,27 @@ class HomePageController extends Controller
 {
     public function getAllProductList()
     {
+          $fData=array();
           $data = DB::table('product_table')->get();
-          return response()->json(['data'=>$data]);
+          for($i=0;$i<sizeof($data);$i++)
+          {
+              $partnerData=DB::table('partner')->where('id',$data[$i]->partner_id)->
+              select('shop_name','address')->get();
+              $childObj=[
+                  'id'=>$data[$i]->id,
+                  'item_name'=>$data[$i]->item_name,
+                  'item_image'=>$data[$i]->item_image,
+                  'price'=>$data[$i]->price,
+                  'price_type'=>$data[$i]->price_type,
+                  'discoutn'=>$data[$i]->discount,
+                  'veg_non_veg'=>$data[$i]->veg_non_veg,
+                  'type'=>$data[$i]->type,
+                  'shop_name'=>$partnerData[0]->shop_name,
+                  'address'=>$partnerData[0]->address
+              ];
+              $fData[$i]=$childObj;
+            //  array_merge($fData,$childObj);
+          }
+          return response()->json(['data'=>$fData]);
     }
 }
