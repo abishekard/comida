425cb4bf44b20dddfe7d51ca8d2d92dfda887ea5
abishek kumar <abishek.ard@gmail.com>
2021-01-27 21:01:39 +0530

    server push

--- app/Http/Controllers/Customer/CInfoController.php
+++ app/Http/Controllers/Customer/CInfoController.php
@@ -73,7 +73,7 @@ class CInfoController extends Controller
 
         return response()->json([
             'status' => 200,
-            'message' => 'updated'
+            'data' => $upl
         ]);
     }
 
