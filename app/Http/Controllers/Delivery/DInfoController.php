dfb99a423555e90b7ca3564ea55fcde06691a1d5
abishek kumar <abishek.ard@gmail.com>
2021-02-13 00:47:25 +0530

    delivery partner profile api done

--- app/Http/Controllers/Delivery/DInfoController.php
+++ app/Http/Controllers/Delivery/DInfoController.php
@@ -18,24 +18,83 @@ class DInfoController extends Controller
 
     public function showProfile(Request $request)
     {
-        $validate= Validator::make($request->all(),[
-            'id'=>'required'
+        $validate = Validator::make($request->all(), [
+            'id' => 'required'
         ]);
-        if($validate->fails())
-        {
+        if ($validate->fails()) {
             return response()->json([
-                'status'=>300,
-                'message'=>$validate->errors()
+                'status' => 300,
+                'message' => $validate->errors()
             ]);
         }
 
-        $data=DB::table('delivery_partner')->where('id',$request->id)->select('name','email',
-        'profile_image','available','aadhar_number','mobile','dob'
+        $data = DB::table('delivery_partner')->where('id', $request->id)->select(
+            'name',
+            'email',
+            'profile_image',
+            'available',
+            'aadhar_number',
+            'mobile',
+            'dob'
         )->get();
 
         return response()->json([
-            'status'=>200,
-            'data'=>$data
+            'status' => 200,
+            'data' => $data
+        ]);
+    }
+
+
+    public function editProfile(Request $request)
+    {
+
+        $validate = Validator::make($request->all(), [
+            'id' => 'required'
+        ]);
+        if ($validate->fails()) {
+            return response()->json([
+                'status' => 300,
+                'message' => $validate->errors()
+            ]);
+        }
+
+        if ($request->profile_image) {
+            $validate = Validator::make($request->all(), [
+                'profile_image' => 'mimes:png,jpg'
+            ]);
+            if ($validate->fails()) {
+                return response()->json([
+                    'status' => 300,
+                    'message' => $validate->errors()
+                ]);
+            }
+            $old_img = DB::table('partner')->where('id', $request->id)
+                ->select('profile_image')->get();
+
+
+            // return response()->json([$old_img]);
+            if ($old_img[0]->profile_image != null)
+                unlink('public/' . $old_img[0]->profile_image);
+
+            $new_img = $request->file('profile_image');
+            $img_name = hexdec(uniqid()) . '.' . 'jpg';
+            $up_location = 'images/delivery_partner_profile_image/';
+            $upl = 'public/images/delivery_partner_profile_image/';
+            $new_img->move($upl, $img_name);
+
+
+            DB::table('delivery_partner')->where('id', $request->id)
+                ->update(['profile_image' => $up_location . $img_name]);
+        }
+
+        if ($request->available == 0 || $request->available == 1) {
+            DB::table('delivery_partner')->where('id', $request->id)
+                ->update(['available' => $request->available]);
+        }
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'updated'
         ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -103,9 +103,11 @@ Route::post('delivery/partner/store/fcm','Delivery\DPartnerController@storeFcmTo
 //Route::post('delivery/partner/login', 'Delivery\DLoginController@login');
 Route::post('delivery/partner/get/address', 'Delivery\DPartnerController@getDeliveryPartnerAddress');
 Route::post('delivery/partner/show/profile', 'Delivery\DInfoController@showProfile');
+Route::post('delivery/partner/edit/profile', 'Delivery\DInfoController@edtProfile');
 Route::post('delivery/partner/get/personal/info', 'Delivery\DPartnerController@getPersonalInfo');
 Route::post('delivery/partner/order/new/{id}', 'Delivery\DOrderController@newOrder');
 Route::post('delivery/partner/order/progress/{id}', 'Delivery\DOrderController@inProgressOrder');
 Route::post('delivery/partner/order/completed/{id}', 'Delivery\DOrderController@completedOrder');
 Route::post('delivery/partner/order/detail/{id}', 'Delivery\DOrderController@orderDetail');
 Route::post('delivery/partner/order/delivered', 'Delivery\DOrderController@orderDelivered');
+
