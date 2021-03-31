3bd6de7ed18df95cede3fc9451e037dec2be9c6d
abishek kumar <abishek.ard@gmail.com>
2021-03-31 22:27:24 +0530

    availability check correction

--- app/Http/Controllers/Partner/PartnerAvailabilityController.php
+++ app/Http/Controllers/Partner/PartnerAvailabilityController.php
@@ -11,35 +11,38 @@ class PartnerAvailabilityController extends Controller
 {
     public function isPartnerAvailableToTakeOrder(Request $request)
     {
-          $validate = Validator::make($request->all(),[
-                "partner_id" => "required",
-                "address_id"=>"required"
-          ]);
+        $validate = Validator::make($request->all(), [
+            "partner_id" => "required",
+            "address_id" => "required"
+        ]);
 
-          if($validate->fails())
-          {
-              return response()->json([
-                  "status"=>300,
-                  "message"=>$validate->errors()
-              ]);
+        if ($validate->fails()) {
+            return response()->json([
+                "status" => 300,
+                "message" => $validate->errors()
+            ]);
+        }
+
+        $customerAddress = DB::table('customeraddresstable')->where('id', $request->address_id)->select('local_city')->first();
+
+        $data = DB::table('partner')->where("id", $request->partner_id)->select("available", "local_city", "open_time", "close_time")->first();
+
+        $available2=0;
+        if (time() > strtotime($data->open_time) && time() < strtotime($data->close_time)) {
+            $available2=1;
           }
 
-          $customerAddress = DB::table('customeraddresstable')->where('id',$request->address_id)->select('local_city')->first();
-
-          $data = DB::table('partner')->where("id",$request->partner_id)->select("available","local_city")->first();
-
-          $addressCheck=0;
-          if($customerAddress->local_city == $data->local_city)
-          {
-               $addressCheck=1;
-          }
-          else{
-              $addressCheck=0;
-          }
-          return response()->json([
-              "status"=>200,
-              "available"=>$data->available,
-              "address_check"=>$addressCheck
-          ]);
+        $addressCheck = 0;
+        if ($customerAddress->local_city == $data->local_city) {
+            $addressCheck = 1;
+        } else {
+            $addressCheck = 0;
+        }
+        return response()->json([
+            "status" => 200,
+            "available1" => $data->available,
+            "available2" => $available2,
+            "address_check" => $addressCheck
+        ]);
     }
 }
